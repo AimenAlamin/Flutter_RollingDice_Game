2e0db8b562637870c80e63b96fd8c94fa4c05d3f
AimenAlamin <alaminaimen@gmail.com>
2024-02-13 07:10:31 +0200

    another way, made parameter to this.intext

--- lib/styled_text.dart
+++ lib/styled_text.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
 
 class StyledText extends StatelessWidget {
-  const StyledText(String intext, {super.key})
-      : outputText = intext; //OUT is our variable, IN is the parameter
+  const StyledText(this.intext,
+      {super.key}); //OUT is our variable, IN is the parameter
   final String
-      outputText; //made it final so the variable argument which is assigned won't change inside this class
+      intext; //made it final so the variable argument which is assigned won't change inside this class
   @override
   Widget build(BuildContext context) {
     return Text(
-      outputText,
-      // textDirection: TextDirection.rtl,
+      intext,
       style: const TextStyle(
         color: Color.fromARGB(255, 248, 248, 248),
         fontSize: 32.0,
